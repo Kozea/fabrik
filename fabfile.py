b236a447c77dc9a6d9d2a8f7c6680c2c7f8665ab
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2013-02-21 13:18:14 +0100

    Use env.hosts instead of roles

--- fabfile.py
+++ fabfile.py
@@ -3,25 +3,24 @@
 from fabric.api import env, sudo, hosts, parallel
 
 env.use_ssh_config = True
-env.roledefs['webservers'] = [
-    'geraldine', 'claire', 'clementine', 'marjorie', 'julia']
+env.hosts = [
+    'geraldine', 'claire', 'clementine', 'marjorie', 'julia', 'sandra',
+    'amandine']
 
 
-@hosts(env.roledefs['webservers'])
 def password():
     sudo('echo "PASSWORD OK"')
 
 
 @parallel
-@hosts(env.roledefs['webservers'])
 def sync():
     if env.host_string not in ('clementine',):
         sudo('layman -S')
     sudo('emerge --sync --quiet')
 
 
-@hosts(env.roledefs['webservers'])
 def update():
     sudo('emerge -DNautvq --keep-going --with-bdeps=y @world')
     sudo('dispatch-conf')
     sudo('revdep-rebuild -- -avq1')
+    sudo('emerge -DNautvq --keep-going @preserved-rebuild')
