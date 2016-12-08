8db08d90f093bbb9598501b701dbe1a98b486b21
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2016-12-08 11:13:41 +0100

    Various improvements

--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python2
 
-from fabric.api import env, sudo, hosts, parallel
+import signal
+
+from fabric.api import env, sudo as fabric_sudo, parallel
+from gi.repository import Notify
+
 
 env.use_ssh_config = True
-env.hosts = [
-    'geraldine', 'claire', 'clementine', 'marjorie', 'julia', 'sandra',
-    'amandine']
+env.hosts = ['geraldine', 'claire', 'cecile', 'fanny', 'melanie']
+
+Notify.init('Fabrik')
+
+
+def sudo(title, task, timeout=120):
+    signal.signal(
+        signal.SIGALRM,
+        lambda signum, frame: Notify.Notification.new(title).show())
+    signal.alarm(timeout)
+    fabric_sudo(task)
+    signal.alarm(0)
 
 
 def password():
-    sudo('echo "PASSWORD OK"')
+    sudo('Saisie du mot de passe', 'echo "PASSWORD OK"', timeout=10)
 
 
 @parallel
 def sync():
-    if env.host_string not in ('clementine',):
-        sudo('layman -S')
-    sudo('emerge --sync --quiet')
+    fabric_sudo('emaint sync -a')
 
 
 def update():
-    sudo('emerge -DNautvq --keep-going --with-bdeps=y @world')
-    sudo('dispatch-conf')
-    sudo('revdep-rebuild -- -avq1')
-    sudo('emerge -DNautvq --keep-going @preserved-rebuild')
+    sudo('Lecture des nouvelles', 'eselect news read')
+    sudo('Mise à jour du serveur', 'emerge -DNautq --keep-going --with-bdeps=y --quiet-build @world')
+    sudo('Mise à jour des configurations', 'dispatch-conf')
+    sudo('Recompilation des paquets', 'revdep-rebuild -q -- -avq1')
+    sudo('Recompilation des paquets préservés', 'emerge -aq --keep-going @preserved-rebuild')
+    sudo('Nettoyage du serveur', 'emerge -aq --depclean')
+
+
+def depclean():
+    sudo('Nettoyage du serveur', 'emerge -avq --depclean')
